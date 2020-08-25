62f6486c299b6979c10eaa92e728786015128b30
iantal <antal.micky@yahoo.com>
2020-08-26 00:52:07 +0300

    Execute linguist on demo repository

--- internal/service/breakdown.go
+++ internal/service/breakdown.go
@@ -1,6 +1,11 @@
 package service
 
 import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+
 	"github.com/hashicorp/go-hclog"
 	"github.com/iantal/ld/protos/ld"
 )
@@ -14,5 +19,17 @@ func NewBreakdown(l hclog.Logger) *Breakdown {
 }
 
 func (b *Breakdown) GetLanguages(projectID string) ([]*ld.Language, error) {
+	executeLinguist("portapps")
 	return []*ld.Language{}, nil
+}
+
+func executeLinguist(repo string) {
+	cmd := exec.Command("github-linguist", "/opt/data/"+repo, "--json")
+	cmdOutput := &bytes.Buffer{}
+	cmd.Stdout = cmdOutput
+	err := cmd.Run()
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+	}
+	fmt.Print(string(cmdOutput.Bytes()))
 }
