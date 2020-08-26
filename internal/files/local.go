8cbd7927b77400dab23d894664dafb6c3c789f65
iantal <antal.micky@yahoo.com>
2020-08-27 02:49:39 +0300

    Minor changes

---
+++ internal/files/local.go
@@ -0,0 +1,130 @@
+package files
+
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
+
+	"golang.org/x/xerrors"
+)
+
+// Local is an implementation of the Storage interface which works with the
+// local disk on the current machine
+type Local struct {
+	maxFileSize int // maximum numbber of bytes for files
+	basePath    string
+}
+
+// NewLocal creates a new Local filesytem with the given base path
+// basePath is the base directory to save files to
+// maxSize is the max number of bytes that a file can be
+func NewLocal(basePath string, maxSize int) (*Local, error) {
+	p, err := filepath.Abs(basePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Local{basePath: p}, nil
+}
+
+// Save the contents of the Writer to the given path
+// path is a relative path, basePath will be appended
+func (l *Local) Save(path string, contents io.Reader) error {
+	// get the full path for the file
+	fp := l.FullPath(path)
+
+	// get the directory and make sure it exists
+	d := filepath.Dir(fp)
+	err := os.MkdirAll(d, os.ModePerm)
+	if err != nil {
+		return xerrors.Errorf("Unable to create directory: %w", err)
+	}
+
+	// if the file exists delete it
+	_, err = os.Stat(fp)
+	if err == nil {
+		err = os.Remove(fp)
+		if err != nil {
+			return xerrors.Errorf("Unable to delete file: %w", err)
+		}
+	} else if !os.IsNotExist(err) {
+		// if this is anything other than a not exists error
+		return xerrors.Errorf("Unable to get file info: %w", err)
+	}
+
+	// create a new file at the path
+	f, err := os.Create(fp)
+	if err != nil {
+		return xerrors.Errorf("Unable to create file: %w", err)
+	}
+	defer f.Close()
+
+	// write the contents to the new file
+	// ensure that we are not writing greater than max bytes
+	_, err = io.Copy(f, contents)
+	if err != nil {
+		return xerrors.Errorf("Unable to write to file: %w", err)
+	}
+
+	return nil
+}
+
+// Get the file at the given path and return a Reader
+// the calling function is responsible for closing the reader
+func (l *Local) Get(path string) (*os.File, error) {
+	// get the full path for the file
+	fp := l.FullPath(path)
+
+	// open the file
+	f, err := os.Open(fp)
+	if err != nil {
+		return nil, xerrors.Errorf("Unable to open file: %w", err)
+	}
+
+	return f, nil
+}
+
+// FullPath returns the absolute path
+func (l *Local) FullPath(path string) string {
+	// append the given path to the base path
+	return filepath.Join(l.basePath, path)
+}
+
+func (l *Local) Unzip(archive, target, name string) error {
+	reader, err := zip.OpenReader(archive)
+	if err != nil {
+		return xerrors.Errorf("Unable to open zip file: %w", err)
+	}
+
+	td := filepath.Join(target, name)
+	if err := os.MkdirAll(td, 0755); err != nil {
+		return xerrors.Errorf("Unable to create target directory: %w", err)
+	}
+
+	for _, file := range reader.File {
+		path := filepath.Join(target, file.Name)
+		if file.FileInfo().IsDir() {
+			os.MkdirAll(path, file.Mode())
+		} else {
+			fileReader, err := file.Open()
+			if err != nil {
+				return xerrors.Errorf("Unable to open file: %w", err)
+			}
+			defer fileReader.Close()
+
+			// create a new file at the path
+			targetFile, err := os.Create(path)
+			if err != nil {
+				return xerrors.Errorf("Unable to create file: %w", err)
+			}
+			defer targetFile.Close()
+
+			if _, err := io.Copy(targetFile, fileReader); err != nil {
+				return xerrors.Errorf("Unable to write target file: %w", err)
+			}
+		}
+	}
+
+	return nil
+}
--- internal/service/breakdown.go
+++ internal/service/breakdown.go
@@ -3,29 +3,112 @@ package service
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 
 	"github.com/hashicorp/go-hclog"
+	"github.com/iantal/ld/internal/data"
+	"github.com/iantal/ld/internal/files"
 	"github.com/iantal/ld/protos/ld"
 )
 
 type Breakdown struct {
 	log      hclog.Logger
 	basePath string
+	rkHost   string
+	store    files.Storage
 }
 
-func NewBreakdown(l hclog.Logger, basePath string) *Breakdown {
-	return &Breakdown{l, basePath}
+func NewBreakdown(l hclog.Logger, basePath, rkHost string, store files.Storage) *Breakdown {
+	return &Breakdown{l, basePath, rkHost, store}
 }
 
 func (b *Breakdown) GetLanguages(projectID string) ([]*ld.Language, error) {
-	return b.executeLinguist("portapps"), nil
+	project, err := b.getProjectName(projectID)
+	if err != nil {
+		b.log.Error("Could not get project name", "projectID", projectID, "err", err)
+		return []*ld.Language{}, nil
+	}
+
+	err = b.downloadRepository(project)
+	if err != nil {
+		b.log.Error("Could not download zip from rk for project", "projectID", projectID, "err", err)
+		return []*ld.Language{}, nil
+	}
+
+	return b.executeLinguist(filepath.Join(projectID, "unzip", project.Name)), nil
+}
+
+func (b *Breakdown) getProjectName(projectID string) (*data.Project, error) {
+	resp, err := http.DefaultClient.Get("http://" + b.rkHost + "/api/v1/projects/" + projectID)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Expected error code 200 got %d", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	project := &data.Project{}
+	err = json.Unmarshal(body, project)
+	if err != nil {
+		return nil, err
+	}
+	return project, nil
+}
+
+func (b *Breakdown) downloadRepository(project *data.Project) error {
+	b.log.Info("Project", "p", project)
+	projectID := project.ProjectID.String()
+	resp, err := http.DefaultClient.Get("http://" + b.rkHost + "/api/v1/projects/" + projectID + "/download")
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Expected error code 200 got %d", resp.StatusCode)
+	}
+
+	b.save(projectID, project.Name, resp.Body)
+	resp.Body.Close()
+
+	return nil
+}
+
+func (b *Breakdown) save(id, projectName string, r io.ReadCloser) {
+	b.log.Info("Save project - storage", "id", id)
+
+	unzippedPath := filepath.Join(id, "unzip")
+
+	zp := id + ".zip"
+	fp := filepath.Join(id, "zip", zp)
+	err := b.store.Save(fp, r)
+
+	if err != nil {
+		b.log.Error("Unable to save file", "error", err)
+	} else {
+		b.log.Info("Unzipping", "id", id, "path", unzippedPath)
+		err := b.store.Unzip(b.store.FullPath(fp), b.store.FullPath(unzippedPath), projectName)
+		if err != nil {
+			b.log.Error("Unable to unzip file", "error", err)
+		}
+	}
 }
 
 func (b *Breakdown) executeLinguist(repo string) []*ld.Language {
 	repoPath := filepath.Join(b.basePath, repo)
+	b.log.Info("Base path", "repo", repoPath)
 	cmd := exec.Command("github-linguist", repoPath, "--json")
 	cmdOutput := &bytes.Buffer{}
 	cmd.Stdout = cmdOutput
